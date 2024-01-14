0afc2dde0de335895a28db5dcac69f5bd23004fa
Edward Nagy <52851669+edwardnagy@users.noreply.github.com>
2024-01-14 02:03:34 +0100

    Fix training time range

--- training_last_year_pipeline.py
+++ training_last_year_pipeline.py
@@ -2,4 +2,4 @@ from model.cluster_time_range import ClusterTimeRange
 from training_pipeline import cluster_papers
 
 
-cluster_papers(ClusterTimeRange.LAST_HALF_YEAR)
+cluster_papers(ClusterTimeRange.LAST_YEAR)
